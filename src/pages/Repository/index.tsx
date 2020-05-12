dbaa1b0d5e84340eb5027dd97609a21cefdfdc05
David <david_borelli@live.com>
2020-05-11 22:42:53 -0300

    Estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import {
+  FiChevronLeft as IconLeft,
+  FiChevronRight as IconRight,
+} from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import * as S from './styled';
 
 interface RepositoryParams {
   repository: string;
@@ -11,7 +19,52 @@ const Repository: React.FC = () => {
   console.log(params.repository);
   return (
     <>
-      <h1>Repository</h1>
+      <S.Header>
+        <S.HeaderLogo src={logo} alt="GitExplorer" />
+        <S.HeaderContainerToBack to="/">
+          <IconLeft size={16} />
+          <S.HeaderContainerToBackText>Voltar</S.HeaderContainerToBackText>
+        </S.HeaderContainerToBack>
+      </S.Header>
+
+      <S.RepositoyInfo>
+        <S.AvatarUser
+          src="https://avatars1.githubusercontent.com/u/21282437?s=460&u=05a9d1b4fc18014623de9473372b9a27bbed7148&v=4"
+          alt="logo"
+        />
+        <S.RepositoyInfoContent>
+          <S.RepositoyInfoTitle>davidborelli/bootcamp</S.RepositoyInfoTitle>
+          <S.RepositoyInfoDescrip>A fucking repo</S.RepositoyInfoDescrip>
+        </S.RepositoyInfoContent>
+      </S.RepositoyInfo>
+      <S.RepoDataContainer>
+        <S.RepoDataItem>
+          <S.RepoDataItemNumber>1808</S.RepoDataItemNumber>
+          <S.RepoDataItemInfo>Stars</S.RepoDataItemInfo>
+        </S.RepoDataItem>
+        <S.RepoDataItem>
+          <S.RepoDataItemNumber>48</S.RepoDataItemNumber>
+          <S.RepoDataItemInfo>Forks</S.RepoDataItemInfo>
+        </S.RepoDataItem>
+        <S.RepoDataItem>
+          <S.RepoDataItemNumber>67</S.RepoDataItemNumber>
+          <S.RepoDataItemInfo>Issues abertas</S.RepoDataItemInfo>
+        </S.RepoDataItem>
+      </S.RepoDataContainer>
+
+      <S.Repositories>
+        <S.RepositoriesItemLink to="/la">
+          <S.RepositoriesItem>
+            <S.RepositoriesItemContent>
+              <S.RepositoriesItemTitle>Nomezao da poha</S.RepositoriesItemTitle>
+              <S.RepositoriesItemDescription>
+                loren ipson mussum cussum pingus
+              </S.RepositoriesItemDescription>
+            </S.RepositoriesItemContent>
+            <IconRight size={20} />
+          </S.RepositoriesItem>
+        </S.RepositoriesItemLink>
+      </S.Repositories>
     </>
   );
 };
---
+++ src/pages/Repository/styled.ts
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const HeaderLogo = styled.img``;
+
+export const HeaderContainerToBack = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #a8a8b3;
+  transition: color 0.2s;
+
+  &:hover {
+    color: #666;
+  }
+`;
+
+export const HeaderContainerToBackText = styled.span`
+  margin-left: 4px;
+`;
+
+export const RepositoyInfo = styled.section`
+  display: flex;
+  align-items: center;
+  margin-top: 80px;
+`;
+
+export const AvatarUser = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+`;
+
+export const RepositoyInfoContent = styled.div`
+  margin-left: 24px;
+`;
+
+export const RepositoyInfoTitle = styled.strong`
+  font-size: 36px;
+  color: #3d3d4d;
+`;
+
+export const RepositoyInfoDescrip = styled.p`
+  font-size: 18px;
+  color: #737380;
+  margin-top: 4px;
+`;
+
+export const RepoDataContainer = styled.div`
+  display: flex;
+  margin-top: 40px;
+`;
+
+export const RepoDataItem = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  &:not(:first-child) {
+    margin-left: 80px;
+  }
+`;
+
+export const RepoDataItemNumber = styled.strong`
+  font-size: 36px;
+  color: #3d3d4d;
+`;
+
+export const RepoDataItemInfo = styled.span`
+  margin-top: 4px;
+  color: #6c6c80;
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+`;
+
+export const RepositoriesItemLink = styled(Link)`
+  background: #fff;
+  border-radius: 5px;
+  width: 100%;
+  padding: 24px;
+  display: block;
+  text-decoration: none;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: translateX(10px);
+  }
+
+  &:not(:first-child) {
+    margin-top: 16px;
+  }
+`;
+
+export const RepositoriesItem = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
+
+export const RepositoriesItemContent = styled.div`
+  margin: 0 16px;
+  flex: 1;
+`;
+
+export const RepositoriesItemTitle = styled.strong`
+  font-size: 20px;
+  color: #3d3d4d;
+`;
+
+export const RepositoriesItemDescription = styled.p`
+  font-size: 18px;
+  color: #a8a8b3;
+  margin-top: 4px;
+`;
